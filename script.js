4a05d1e74b8ff92c14aec852849dcb47a3ebd3b9
ej94 <amefawg66@gmail.com>
2019-01-29 14:44:37 +0800

    challenge

---
+++ script.js
@@ -0,0 +1,48 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var span= document.getElementsByTagName("span");
+
+/*toggle .done property*/
+for(var i=0; i<span.length; i++){
+span[i].addEventListener("click", liClick);
+}
+function liClick(){
+  this.classList.toggle("done");
+}
+/*Delete button */
+[...document.getElementsByTagName('button')].forEach(el =>
+  el.addEventListener('click', () =>
+    el.parentNode.parentNode.removeChild(el.parentNode)));
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+    var deletebutton = document.createElement("button")
+    var spanitem = document.createElement("span")
+	spanitem.appendChild(document.createTextNode(input.value));
+    deletebutton.appendChild(document.createTextNode("Delete"));
+	ul.appendChild(li);
+    li.appendChild(spanitem)
+    li.appendChild(deletebutton);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
